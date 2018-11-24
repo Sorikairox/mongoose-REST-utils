db2ab1fed81ee3ce1dd814af54a0cf407f43ab04
Thomas Cruveilher <sorikairox@gmail.com>
2018-11-24 15:07:19 +0100

    added error handler to post

--- utils/basicPost.js
+++ utils/basicPost.js
@@ -3,22 +3,22 @@
  */
 
 
-const basicPost = function (req, res,  Model, next) {
-    if (req.body.data) {
-        var newObject = new Model(req.body.data);
-        newObject.save(function (err) {
-            if (err) throw err;
-            if (next) {
-                return next(req, res, newObject);
-            }
-            else {
-                return res.json({success: true, data: newObject});
-            }
-        });
-    }
-    else {
-        return res.status(400).json({success: false, message: utils_message.MISSING_OBJECT});
-    }
+const basicPost = function (req, res, Model, next) {
+  if (req.body.data) {
+    var newObject = new Model(req.body.data);
+    newObject.save(function (err) {
+      if (err) return res.status(500).json({success: false, data: err});
+      if (next) {
+        return next(req, res, newObject);
+      }
+      else {
+        return res.json({success: true, data: newObject});
+      }
+    });
+  }
+  else {
+    return res.status(400).json({success: false, message: utils_message.MISSING_OBJECT});
+  }
 };
 
 module.exports = basicPost;
