062ba9cea7935d0f0884975c9a298b3be8aebbc5
Thomas Cruveilher <sorikairox@gmail.com>
2018-12-26 11:07:51 +0100

    removed utils message

--- utils/basicPost.js
+++ utils/basicPost.js
@@ -3,22 +3,22 @@
  */
 
 
-const basicPost = function (req, res, Model, next) {
-  if (req.body) {
-    var newObject = new Model(req.body);
-    newObject.save(function (err) {
-      if (err) return res.status(500).json({success: false, data: err});
-      if (next) {
-        return next(req, res, newObject);
-      }
-      else {
-        return res.json({success: true, data: newObject});
-      }
-    });
-  }
-  else {
-    return res.status(400).json({success: false, message: utils_message.MISSING_OBJECT});
-  }
+const basicPost = function(req, res, Model, next) {
+    if (req.body) {
+        var newObject = new Model(req.body);
+        newObject.save(function(err) {
+            if (err) return res.status(500).json({success: false, data: err});
+            if (next) {
+                return next(req, res, newObject);
+            }
+            else {
+                return res.json({success: true, data: newObject});
+            }
+        });
+    }
+    else {
+        return res.status(404).json({success: false, data: {error: {message: 'RequestBodyMissing'}}});
+    }
 };
 
 module.exports = basicPost;
--- utils/basicPut.js
+++ utils/basicPut.js
@@ -2,21 +2,21 @@
  * Created by Sorikairo on 17/08/2016.
  */
 
-const basicPut = function (req, res, Model, next) {
-  if (req.body) {
-    Model.findByIdAndUpdate(req.body._id, req.body, {new: true}, function (err, updatedObject) {
-      if (err) return res.status(500).json({success: false, data: err});
-      if (next) {
-        return next(req, res, updatedObject);
-      }
-      else {
-        return res.json({success: true, data: updatedObject});
-      }
-    });
-  }
-  else {
-    return res.status(400).json({success: false, message: utils_message.MISSING_OBJECT});
-  }
+const basicPut = function(req, res, Model, next) {
+    if (req.body) {
+        Model.findByIdAndUpdate(req.body._id, req.body, {new: true}, function(err, updatedObject) {
+            if (err) return res.status(500).json({success: false, data: err});
+            if (next) {
+                return next(req, res, updatedObject);
+            }
+            else {
+                return res.json({success: true, data: updatedObject});
+            }
+        });
+    }
+    else {
+        return res.status(404).json({success: false, data: {error: {message: 'RequestBodyMissing'}}});
+    }
 };
 
 module.exports = basicPut;
